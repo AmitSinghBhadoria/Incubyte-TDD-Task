ec093dadbac510e30370439dfec13c6bfee65f16
Amit Singh Bhadoria <amit@gyde.ai>
2024-10-15 19:18:16 +0530

    Refactor input string handling and add new test cases
    - Refactored the input string processing: replaced newline characters with commas before splitting the string into an array of numbers.
    - Added test case to handle an unknown number of numbers.
    - Added test case to handle newlines between numbers.

--- calculator.js
+++ calculator.js
@@ -2,8 +2,10 @@ class StringCalculator {
     Add(numbers) {
         // if numbers have a falsy value return 0
         if (!numbers) return 0;
+        // Split Numbers String by \n and join to create a new string with only comma seperated value.
+        const commaSeperatedString = numbers.split("\n").join(",");
         // Split Numbers String by , and store it in an array of numbers.
-        const numArray = numbers.split(',').map(Number);
+        const numArray = commaSeperatedString.split(",").map(Number);
         // add all the numbers in the num Array
         return numArray.reduce((acc, curr) => acc + curr, 0);
     }
--- calculator.test.js
+++ calculator.test.js
@@ -7,12 +7,18 @@ describe('StringCalculator', () => {
 
         it('should return 0 for an empty string', () => {
             expect(sc.Add("")).toBe(0);
-          });
+        });
         it("should return the number for a single input", () => {
             expect(sc.Add("1")).toBe(1);
         })
         it("should return the sum of two comma-separated numbers", () => {
             expect(sc.Add("1,2")).toBe(3);
+        })
+        it('should handle an unknown number of numbers', () => {
+            expect(sc.Add("1,2,3,4,5")).toBe(15);
+        });
+        it("should handle new lines between numbers", () => {
+            expect(sc.Add("1\n2,3")).toBe(6);
         })
     })
 })
